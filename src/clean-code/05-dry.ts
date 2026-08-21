9c3d819f2716241a16eed4fa3fc641867f051214
johhcristobal <johnalexiscristobaljimenez6@gmail.com>
2026-08-21 17:15:01 -0600

    s2 finished

---
+++ src/clean-code/05-dry.ts
@@ -0,0 +1,42 @@
+type Size = 'S' | 'M' | 'L' | 'XL' | '';
+
+class Product {
+    constructor(
+        public name: string = '',
+        public price: number = 0,
+        public size: Size = ''
+    ) {}
+
+    isProductReady(): boolean {
+        for(const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if(this[key].length <= 0) throw new Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if(this[key] <= 0) throw new Error(`${key} is zero`);
+                    break;
+                default:
+                    throw new Error(`${typeof this[key]} is not supported`);
+            }
+        }
+        return true;
+    }
+
+    toString() {
+        // No Dry
+        // if (this.name.length <= 0) throw new Error('Name is empty');
+        // if (this.price <= 0) throw new Error('Price is zero');
+        // if (this.size.length <= 0) throw new Error('Size is empty');
+
+        // Dry
+        
+
+        return `${this.name} ($${this.price}), ${this.size}`;
+    }
+}
+
+(() => { 
+    const bluePants = new Product('Blue large pants', 10, 'L');
+    console.log(bluePants.toString());
+})();
